0848513e73bc1c8b6ddc8f5a7ee9b37260299771
unknown <s-2508657@AVI109162LD.ERAC.HSD.PRI>
2014-10-16 16:43:38 -0700

    First commit to Heroku

---
+++ app.js
@@ -0,0 +1,67 @@
+console.log("Started program");
+var sqlite = require('sqlite3').verbose();
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+var morgan = require('morgan');
+var path = require('path');
+
+app.listen(5000);
+console.log("required all modules");
+
+function handler (req, res) {
+  console.log("Starting request...");
+  var filePath = "." + req.url;
+  if(filePath == './') { filePath = './index.html'}
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.html':
+      contentType = 'text/html';
+  }
+  path.exists(filePath, function(exists){
+      if (exists) {
+          fs.readFile(filePath, function(error, content) {
+              if(error) {
+                  res.writeHead(500);
+                  res.end();
+              }
+              else {
+                res.writeHead(200, {'Content-Type': contentType});
+                res.end(content, 'utf-8');
+              }
+          });
+      }
+      else {
+          res.writeHead(404);
+          res.end();
+      }
+  });
+}
+console.log('Got this far');
+
+
+file = 'db.sqlite';
+var db = new sqlite.Database(file);
+var dbexists = fs.existsSync(file);
+
+db.serialize(function() { 
+    if(!dbexists) {
+        db.run("CREATE TABLE scouting (tablet INTEGER, match INTEGER, team INTEGER, alliance1 INTEGER, alliance2 INTEGER, deadbot BOOL, noshow BOOL, fataljam BOOL, startingposition TEXT, autopoints INTEGER, autogoals INTEGER, automoved INTEGER, kickstand BOOL, teleballs INTEGER, telestyle TEXT, teleshort INTEGER, telemedium INTEGER, telelarge INTEGER, endcenter INTEGER, endfinal INTEGER, results STRING)");
+    }
+});
+
+io.sockets.on('connection', function(socket) {
+  socket.on('addEntry', function(data) {
+    console.log("Database entry emit");
+    db.serialize(function() {
+        console.log(data.team);
+        db.run("INSERT INTO scouting VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [data.tablet, data.match, data.team, data.alliance1, data.alliance2, data.deadbot, data.noshow, data.fataljam, data.startingposition, data.autopoints, data.autogoals, data.automoved, data.kickstand, data.telestyle, data.teleshort, data.telemedium, data.telelarge, data.endcenter, data.endfinal, data.results]);
+    });
+  });
+})
+
+    
---
+++ index.js
@@ -0,0 +1,56 @@
+var socket;
+
+socket = io.connect("localhost:5000");
+
+function show(id) { //Makes sure the match selection isn't shown until they've selected a match
+    document.getElementById(id).style.visibility= "visible"; //Shows entire div match
+}
+
+function hide(id) { //Called body onLoad, hides matches from view
+    document.getElementById(id).style.visibility= "hidden"; //Hides entire div match
+}
+
+function load() {
+    hide("match");
+    hide("button");
+}
+
+function sendData() {
+	data = {};
+	data.match = null;
+	data.tablet = $('#selTab').val();
+	data.team = $('#selMatch').val();
+	data.alliance1 = null;
+	data.alliance2 = null;
+	data.deadbot = null;
+	data.noshow = null;
+	data.fataljam = null;
+	data.startingposition = null;
+	data.autopoints = null;
+	data.autogoals = null;
+	data.automoved = null;
+	data.kickstand = null
+	data.telestyle = null;
+	data.teleshort = null;
+	data.telemedium = null;
+	data.telelarge = null;
+	data.endcenter = null;
+	data.endfinal = null;
+	data.results = null;
+	console.log(data);
+	socket.emit('addEntry', data);
+        window.location.href = "game.html";
+}
+$(document).ready( function(){ //Makes sure the html is ready before looking for #selTab
+
+$("#selTab").change(function() {
+  show("match");
+});
+$("selMatch").change(function() {
+  show("bottomRight");
+});
+
+});
+
+/* data.tablet, data.match, data.team, data.alliance1, data.alliance2, data.deadbot, data.noshow, data.fataljam, data.startingposition, data.autopoints, data.autogoals, data.automoved, data.kickstand, data.telestyle, 
+   data.teleshort, data.telemedium, data.telelarge, data.endcenter, data.endfinal, data.results*/
